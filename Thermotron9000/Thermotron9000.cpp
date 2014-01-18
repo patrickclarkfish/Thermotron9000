5e9170f9e0380de2d215fdd5e53f4ca4f4a26323
Patrick Clark <patrickclarkfish@gmail.com>
2014-01-18 15:53:02 -0600

    Initial program (no code).
    Empty files.

---
+++ Thermotron9000/Thermotron9000.cpp
@@ -0,0 +1,17 @@
+/*
+ * Thermotron9000.cpp
+ *
+ * Created: 1/18/2014 2:36:10 PM
+ *  Author: Patrick
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
