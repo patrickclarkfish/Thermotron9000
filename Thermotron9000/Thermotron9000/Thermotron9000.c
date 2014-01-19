8ff2612d281780cd1a2e16855b71e76a9ecd0c7c
Patrick Clark <patrickclarkfish@gmail.com>
2014-01-18 20:56:01 -0600

    Changed to c project.
    Most of my libraries are for c.

---
+++ Thermotron9000/Thermotron9000/Thermotron9000.c
@@ -0,0 +1,38 @@
+/*
+ * Thermotron9000.c
+ * Created: 1/18/2014 2:36:10 PM
+ * Thermostat Firmware for AVR
+ * Copyright (C) 2014  Patrick R. Clark
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *   
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Patrick Clark
+ * 2916 Heathrow Drive
+ * Unit 56
+ * Ames, IA 50014
+ * 
+ * Patrick@prclark.net
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
